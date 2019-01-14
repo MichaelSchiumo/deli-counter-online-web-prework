76287fc31167f5efb4ce24ae4660d36b0ed9e8e0
Michael J. Schiumo <mschiumo18@gmail.com>
2019-01-14 21:28:32 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,20 +1,24 @@
+
+
 def line(katz_deli)
   i = katz_deli.length 
   if i == 0 
     puts "The line is currently empty."
+  
   elsif i > 0 
-     deli_line = ""
-     katz_deli.each_with_index do |name, index|
-          deli_line += "#{index + 1}. #{name} "
-     end 
-     puts "The line is currently: #{deli_line.strip}"
+    deli_line = ""
+    katz_deli.each_with_index do |name, index|
+      deli_line += "#{index + 1}. #{name} "
+    end 
+    puts "The line is currently: #{deli_line.strip}"
   end
+  
 end 
 
 
 def take_a_number(line, name)
-line.push(name)
-  puts "Welcome, #{name}. You are number #{line.length} in line."
+  line.push(name)
+    puts "Welcome, #{name}. You are number #{line.length} in line."
 end
 
 
@@ -23,8 +27,7 @@ def now_serving(katz_deli)
   i = katz_deli.length
     if i == 0
       puts "There is nobody waiting to be served!"
-    elsif i > 0 
-      puts "Currently serving #{katz_deli.shift}."
-end      
+        elsif i > 0 
+          puts "Currently serving #{katz_deli.shift}."
+    end      
 end
-
